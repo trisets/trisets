1b1e12d1c327757f7d89901fbe598af1b68fed7a
terra-d <38385364+terra-d@users.noreply.github.com>
2022-11-18 00:32:22 +0900

    ci: coverageのクライテリアを設定

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    coverage:{
+      all: true,
+      exclude: ['.next/*', '**/*.d.ts', '*.config.{js,ts}', '**/__test__/*'],
+      statements: 80,
+      branches: 80,
+      functions: 100,
+      lines: 80,
+    }
   },
 });
