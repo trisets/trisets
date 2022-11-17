a0dfec12e25153912096109c1d1f3718e5593876
terra-d <38385364+terra-d@users.noreply.github.com>
2022-11-17 22:15:00 +0900

    style: 返り値の定義を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
   // eslint-disable-next-line react/jsx-props-no-spreading
   <Component {...pageProps} />
 );
