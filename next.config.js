4d61d1bd8a6e90c5dedc134482c9bbbcff351278
habutaso <habutaso@gmail.com>
2022-11-19 18:30:44 +0900

    fix: productionモードの時^data-testタグを消す

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    reactRemoveProperties: process.env.NODE_ENV === 'production'
+  }
 };
 
 module.exports = nextConfig;
