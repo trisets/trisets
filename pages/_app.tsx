835caa1fc27846e53715fd589c3bbd7ee8ad27ce
banquet <26418141+banquet27@users.noreply.github.com>
2022-11-19 15:47:10 +0900

    feat: implement header-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
+import HeaderBar from '../components/HeaderBar';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
+  <div>
+    <HeaderBar />
+    <Component {...pageProps} />
+  </div>
 );
 
 export default MyApp;
