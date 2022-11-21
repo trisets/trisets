cb19b0c0d17849eb451bfd71642aad51cd2a25a4
terra-d <38385364+terra-d@users.noreply.github.com>
2022-11-22 01:55:06 +0900

    ci: add test

--- __test__/api/Hello.test.ts
+++ __test__/api/Hello.test.ts
@@ -1,11 +1,18 @@
 import hello from '@/pages/api/hello';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { sendStatusCode } from 'next/dist/server/api-utils';
 import { expect, test } from 'vitest';
 
 test('hello', async () => {
   const req = {} as NextApiRequest;
-  const res = {} as NextApiResponse;
+  const res = {
+    status: (statusCode: number): object => sendStatusCode(res, statusCode),
+  } as NextApiResponse<object>;
+  let resData: object = {};
+  res.json = (data: object): void => {
+    resData = data;
+  };
 
   hello(req, res);
-  expect(res).toEqual({ name: 'John Doe' });
+  expect(resData).toEqual({ name: 'John Doe' });
 });
