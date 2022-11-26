cc2f11de8db98f9893d0c785711ad385408a596e
terra-d <38385364+terra-d@users.noreply.github.com>
2022-11-27 03:03:51 +0900

    fix: fetchを上書きする方針に変更

--- __test__/vitest.setup.ts
+++ __test__/vitest.setup.ts
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { afterAll, afterEach, beforeAll } from 'vitest';
-import 'whatwg-fetch';
+// NOTE: いつか組み込みのfetchを利用する方針に変えられるかも
+import { fetch } from 'whatwg-fetch';
 import { server } from '../mocks/server';
+
+globalThis.fetch = fetch;
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterAll(() => server.close());
